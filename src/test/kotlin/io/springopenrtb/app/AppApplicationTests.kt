b8543924421cb27ef9995b84c691df087a10e470
SunMyeong Lee <actumn814@gmail.com>
2020-11-02 01:02:54 +0900

    Initial commit

---
+++ src/test/kotlin/io/springopenrtb/app/AppApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.springopenrtb.app
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
